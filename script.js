da29ba20d4657357beb721a8e0c05caf429892a4
shikhar <shikharagarwale7@gmail.com>
2023-01-08 16:32:28 +0530

    withdraw func added and minor changes

--- script.js
+++ script.js
@@ -4,9 +4,26 @@ import { contractAddress, abi } from "./constants.js";
 
 const connectButton = document.getElementById("connectButton");
 const fundButton = document.getElementById("fundButton");
+const withdrawButton = document.getElementById("withdrawButton");
+const balanceButton = document.getElementById("balanceButton");
 
 connectButton.onclick = connect;
 fundButton.onclick = fund;
+withdrawButton.onclick = withdraw;
+balanceButton.onclick = getBalance;
+
+// const inputEthAmt = document.getElementById("ethAmt");
+// inputEthAmt.onchange = checkNum;
+
+// function checkNum() {
+//     const value = inputEthAmt.value.toString();
+//     if (value=="0") {
+//         document.getElementById("fundResponse").innerHTML = "Please enter Amt";
+//     } 
+//     else {
+//         document.getElementById("fundResponse").innerHTML = "";
+//     }
+// }
 
 async function connect() {
     if (typeof window.ethereum !== "undefined") {
@@ -26,10 +43,24 @@ async function connect() {
     }
 }
 
+async function getBalance() {
+    if (typeof window.ethereum!=="undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const balance = await provider.getBalance(contractAddress);
+        document.getElementById("balanceButtonResponse").innerHTML = `${balance/1e18} ETH`;
+    }
+}
+
 async function fund() {
     if (typeof window.ethereum !== "undefined") {
+        const inputEthAmt = document.getElementById("ethAmt");
+        let value = inputEthAmt.value.toString();
+        if (value=="") {
+            document.getElementById("fundResponse").innerHTML = "Please enter Amt";
+            return;
+        }
         document.getElementById("fundResponse").innerHTML = "Funding...";
-        const ethAmt = ethers.utils.parseEther("1");
+        const ethAmt = ethers.utils.parseEther(value);
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
         const fundMe = new ethers.Contract(contractAddress, abi, signer);
@@ -47,19 +78,53 @@ async function fund() {
             if (e.message.toLowerCase().includes("user denied transaction")) {
                 document.getElementById("fundResponse").innerHTML = "Txn was rejected by user";
             }
+            else if (e.data.message.toLowerCase().includes("amount should be atleast 1 eth")) {
+                document.getElementById("fundResponse").innerHTML = "Amount should be atleast 1 eth";
+            }
             else {
                 document.getElementById("fundResponse").innerHTML = "Txn was terminated due to an error";
                 console.log(e);
             }
         }   
     }
+    document.getElementById("ethAmt").value = "";
 }
 
-function hashMining(fundResponse, provider) {
-    console.log(`Mining ${fundResponse.hash} ....`);
+async function withdraw() {
+    const withdrawMessage = document.getElementById("withdrawMessage");
+    if (typeof window.ethereum !== "undefined") {
+        withdrawMessage.innerHTML = "Withdrawing....";
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const fundMe = new ethers.Contract(contractAddress, abi, signer);
+        try {
+            const before = await fundMe.provider.getBalance(fundMe.address);
+            if (before=="0") {
+                withdrawMessage.innerHTML = "The Amt is 0 ETH";
+                return;
+            }
+            const txnResponse = await fundMe.withdraw();
+            await hashMining(txnResponse, provider);
+            const after = await fundMe.provider.getBalance(fundMe.address);
+            console.log(`Amt withdrawn: ${(before-after)/1e18} ETH`);
+            withdrawMessage.innerHTML = "Successfully Withdrawn";
+        }
+        catch(e) {
+            withdrawMessage.innerHTML = "Ah-Snap! Caught an error";
+            console.log(e);
+        }
+    }
+    else {
+        withdrawMessage.innerHTML = "Uh-oh! Unable to detect Metamask";
+    }
+
+}
+
+function hashMining(txnResponse, provider) {
+    console.log(`Mining ${txnResponse.hash} ....`);
     return new Promise((resolve, reject) => {
-        provider.once(fundResponse.hash, (fundReceipt) => {
-            console.log(`Total: ${fundReceipt.confirmations}`);
+        provider.once(txnResponse.hash, (txnReceipt) => {
+            console.log(`Total Confirmarions: ${txnReceipt.confirmations}`);
             resolve();
         });
     });
